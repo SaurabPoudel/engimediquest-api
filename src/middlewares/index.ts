df98cd3afdc03c187265f744474d44872614db42
SaurabPoudel <poudelsaurab20@gmail.com>
2023-04-14 21:14:16 +0545

    Sign In and Login User Updating username of user done

---
+++ src/middlewares/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import { get, merge } from "lodash";
+
+import { getUserBySessionTOken } from "../db/users";
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity._id") as unknown as string;
+
+    if (!currentUserId || currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const isAuthenticated = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const sessionToken = req.cookies["ENGIMEDIQUEST_SESSION_TOKEN"];
+    if (!sessionToken) {
+      return res.sendStatus(403);
+    }
+    const existingUser = await getUserBySessionTOken(sessionToken);
+    if (!existingUser) {
+      return res.sendStatus(403);
+    }
+    merge(req, { identity: existingUser });
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
